c609a79d5ddf4491da7ed812f564b0dd3f46005e
Daniil Vdovin <45402557+DaniilVdovin@users.noreply.github.com>
2020-03-20 00:16:50 +0300

    map poligin

--- lib/Data.dart
+++ lib/Data.dart
@@ -24,7 +24,7 @@ class User{
 }
 class Data {
   static SharedPreferences prefs;
-  static String serverprefix = "http://localhost:15950/";
+  static String serverprefix = "http://192.168.1.3:15950/";
   static String token;
 
   static City city = City(name:"Адлер",coordinats: [43.5832281,39.991644]);
--- lib/pages/Mainmapscreen.dart
+++ lib/pages/Mainmapscreen.dart
@@ -7,7 +7,6 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 
-
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:toast/toast.dart';
 import '../Data.dart';
@@ -17,164 +16,218 @@ class Mainmapscreen extends StatefulWidget {
   @override
   MainmapscreenStage createState() => MainmapscreenStage();
 }
-void _loadTopicData(){
+
+void _loadTopicData() {
   Data.topic = Topic(
-  fullname:"Mike Marelo",
-  discription: "Lost aroun KFC рядом видели бабушку",
-  price: "1.000.000.000P", 
-  imageUrl:Data.serverprefix + "api/getImage?token="+Data.token+"&file=def.png");
+      fullname: "Mike Marelo",
+      discription: "Lost aroun KFC рядом видели бабушку",
+      price: "1.000P",
+      imageUrl: Data.serverprefix +
+          "api/getImage?token=" +
+          Data.token +
+          "&file=def.png");
 }
 
 class MainmapscreenStage extends State<Mainmapscreen> {
-    void _loadMe() async {
-     await http.get(Data.serverprefix+'api/getme?token='+Data.token)
-        .then((response){
-          Map<String,dynamic> js = json.decode(utf8.decode(response.bodyBytes));
-          Data.user = User(
-            id:       js["id"],
-            fullname: js["fullname"],
-            login:    js["login"],
-            location: js["location"],
-            verified: js["verified"],
-            searches: js["searches"],
-            avatar:   js["avatar"]
-            );
-          setState(() {
-            Toast.show("r:"+utf8.decode(response.bodyBytes), context, duration: Toast.LENGTH_LONG, gravity:  Toast.BOTTOM);
-          });
-        });
-    }
+  void _loadMe() async {
+    await http
+        .get(Data.serverprefix + 'api/getme?token=' + Data.token)
+        .then((response) {
+      Map<String, dynamic> js = json.decode(utf8.decode(response.bodyBytes));
+      Data.user = User(
+          id: js["id"],
+          fullname: js["fullname"],
+          login: js["login"],
+          location: js["location"],
+          verified: js["verified"],
+          searches: js["searches"],
+          avatar: js["avatar"]);
+      setState(() {
+        Toast.show("r:" + utf8.decode(response.bodyBytes), context,
+            duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
+      });
+    });
+  }
 
   @override
   void initState() {
     super.initState();
     _loadTopicData();
     _loadMe();
-   }
-    
+  }
+
   @override
   Widget build(BuildContext context) {
     TextStyle bodyStyle = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
     TextStyle tistleStyle = bodyStyle.copyWith(fontSize: 30.0);
 
     final hmfButton = Material(
-          elevation: 2.0,
-          borderRadius: BorderRadius.circular(15.0),
-          color: Colors.white,
-          child: MaterialButton(
-            minWidth: MediaQuery.of(context).size.width,
-            padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-            onPressed: null,
-            child: Text("Help me find !",
-                textAlign: TextAlign.center,
-                style: bodyStyle.copyWith(
-                    color: Colors.black45, fontWeight: FontWeight.bold)),
-            ),   
-        );
+      elevation: 2.0,
+      borderRadius: BorderRadius.circular(15.0),
+      color: Colors.white,
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        onPressed: null,
+        child: Text("Help me find !",
+            textAlign: TextAlign.center,
+            style: bodyStyle.copyWith(
+                color: Colors.black45, fontWeight: FontWeight.bold)),
+      ),
+    );
     final profButton = FloatingActionButton(
       elevation: 2.0,
-      onPressed: (){
-        Navigator.push(context,
-        MaterialPageRoute(builder:  (context)=> Userpage()));
+      onPressed: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => Userpage()));
       },
-      child: Icon(Icons.person_outline,size: 30.0,),
+      child: Icon(
+        Icons.person_outline,
+        size: 30.0,
+      ),
       backgroundColor: Colors.white,
       foregroundColor: Colors.black54,
-      );
+    );
     final nawcard = SafeArea(
         child: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: 
-              Container(
-                height: MediaQuery.of(context).size.height*0.135,
-                child: Material(
-                  elevation: 2.0,
-                  borderRadius: BorderRadius.circular(15.0),
-                  color: Colors.white,
-                  child: Container(
-                    margin: EdgeInsets.all(10.0),
-                    child:Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                         ClipRRect(
-                         borderRadius: BorderRadius.circular(15.0),
-                         child: Image.network(
-                         Data.topic.imageUrl,
-                         width: 100,
-                         height: 100,
-                         ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 10.0),
-                          child: Stack(
-                              overflow: Overflow.visible,
-                              children:[Column(
-                              mainAxisSize: MainAxisSize.min,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text(Data.topic.fullname,style: tistleStyle.copyWith(fontWeight: FontWeight.w800,fontSize: 18.0)),
-                                SizedBox(height: 5.0),
-                                Container(
-                                  width: MediaQuery.of(context).size.width*0.6,
-                                  child: Text(Data.topic.discription,style: bodyStyle.copyWith(fontSize: 16.0) ,textAlign: TextAlign.left),
-                                ),
-                              ],
-                            ),
-                             Align(alignment: Alignment.bottomRight,
-                               child: Text(Data.topic.price,
-                               style: tistleStyle.copyWith(color: Colors.black54,fontWeight: FontWeight.w800,fontSize: 18.0)))]),
-                        )
-                      ],
+      padding: const EdgeInsets.all(15.0),
+      child: Container(
+        height: MediaQuery.of(context).size.height * 0.135,
+        child: Material(
+            elevation: 2.0,
+            borderRadius: BorderRadius.circular(15.0),
+            color: Colors.white,
+            child: Container(
+              margin: EdgeInsets.all(10.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(15.0),
+                    child: Image.network(
+                      Data.topic.imageUrl,
+                      width: 100,
+                      height: 100,
                     ),
-                  )
-          ),
-              ),
-        ));
-    final ui = SafeArea(
-                child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Container(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Align(alignment: Alignment.bottomRight,
-                        child: Padding(
-                          padding: const EdgeInsets.only(bottom: 10.0),
-                          child: profButton,
-                        ),
-                      ),
-                      Align(alignment: Alignment.bottomRight,
-                        child: Padding(padding: const EdgeInsets.symmetric(horizontal: 65.0),
-                          child: hmfButton,
-                        ),
-                      ),
-                    ],
                   ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10.0),
+                    child: Stack(overflow: Overflow.visible, children: [
+                      Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(Data.topic.fullname,
+                              style: tistleStyle.copyWith(
+                                  fontWeight: FontWeight.w800, fontSize: 18.0)),
+                          SizedBox(height: 5.0),
+                          Container(
+                            width: MediaQuery.of(context).size.width * 0.6,
+                            child: Text(Data.topic.discription,
+                                style: bodyStyle.copyWith(fontSize: 16.0),
+                                textAlign: TextAlign.left),
+                          ),
+                        ],
+                      ),
+                      Align(
+                          alignment: Alignment.bottomRight,
+                          child: Text(Data.topic.price,
+                              style: tistleStyle.copyWith(
+                                  color: Colors.black54,
+                                  fontWeight: FontWeight.w800,
+                                  fontSize: 18.0)))
+                    ]),
+                  )
+                ],
+              ),
+            )),
+      ),
+    ));
+    final ui = SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Align(
+                alignment: Alignment.bottomRight,
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 10.0),
+                  child: profButton,
                 ),
               ),
-            );
-    
+              Align(
+                alignment: Alignment.bottomRight,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 65.0),
+                  child: hmfButton,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+
     Completer<GoogleMapController> _controller = Completer();
+
     LatLng _center = LatLng(Data.city.coordinats[0], Data.city.coordinats[1]);
+    Set<Circle> circles = Set.from([
+      Circle(
+        circleId: CircleId(Data.city.name),
+        fillColor: Color.fromARGB(50, 0, 0, 200),
+        consumeTapEvents: true,
+        strokeColor: Colors.blue,
+        onTap: null,
+        strokeWidth: 1,
+        center: _center,
+        radius: 500,
+      )
+    ]);
+
+    Set<Marker> markers = Set.from([
+      Marker(
+        markerId: MarkerId(Data.city.name),
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueAzure),
+        infoWindow: InfoWindow(
+          title: Data.topic.fullname,
+        ),
+        position: _center,
+        )
+    ]);
+    Set<Marker> markers = Set.from([  
+      Marker(
+        markerId: MarkerId(Data.city.name),
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueAzure),
+        infoWindow: InfoWindow(
+          title: Data.topic.fullname,
+        ),
+        position: _center,
+        )
+    ]);
     void _onMapCreated(GoogleMapController controller) {
       _controller.complete(controller);
     }
 
     final map = GoogleMap(
-          onMapCreated: _onMapCreated,   
-         initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 11.0,
-          ));
+      mapType: MapType.hybrid,
+      onMapCreated: _onMapCreated,
+      initialCameraPosition: CameraPosition(
+        target: _center,
+        zoom: 11.0,
+      ),
+      circles: circles,
+      markers: markers,
+    );
 
     return Scaffold(
-          body: Stack(children:[
-                        map,
-                        ui,
-                        nawcard,
-                      ]) 
-        );
+        body: Stack(children: [
+      map,
+      ui,
+      nawcard,
+    ]));
   }
 }
